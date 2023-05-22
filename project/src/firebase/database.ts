66116c0de8e423bfc956d6caefdd27e4c0643067
Huu Nguyen <thieuvan0405@gmail.com>
2023-05-22 15:27:02 +0700

    update using firebase database

--- project/src/firebase/database.ts
+++ project/src/firebase/database.ts
@@ -1,11 +1,11 @@
 import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
 
 const {
+  VITE_APP_DB_URL,
   VITE_APP_FIREBASE_API_KEY,
   VITE_APP_FIREBASE_AUTH_DOMAIN,
   VITE_APP_FIREBASE_PROJECT_ID,
-  VITE_APP_FIREBASE_STORAGE_BUCKET,
   VITE_APP_FIREBASE_APP_ID,
 } = import.meta.env;
 
@@ -13,13 +13,12 @@ const firebaseConfig = {
   apiKey: VITE_APP_FIREBASE_API_KEY,
   authDomain: VITE_APP_FIREBASE_AUTH_DOMAIN,
   projectId: VITE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: VITE_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: '96084632417',
   appId: VITE_APP_FIREBASE_APP_ID,
+  databaseURL: VITE_APP_DB_URL,
 };
 
 const app = initializeApp(firebaseConfig);
 
-const storage = getStorage(app);
+const database = getDatabase(app);
 
-export default storage;
+export default database;
--- project/src/firebase/file.ts
+++ project/src/firebase/file.ts
@@ -1,24 +1,15 @@
-import { getDownloadURL, ref, uploadString } from 'firebase/storage';
-import storage from './storage';
-
-const { VITE_APP_FIREBASE_FILE } = import.meta.env;
+import { push, ref, set } from 'firebase/database';
+import database from './database';
 
 const trigger = (name: string, contact: string) => {
-  const fileRef = ref(storage, VITE_APP_FIREBASE_FILE);
-  return getDownloadURL(fileRef)
-    .then(url => {
-      fetch(url).then(res => {
-        res.text().then(text => {
-          let append = `${new Date().toLocaleDateString()},${name},${contact}`;
-          append = `${text}
-${append}`;
-          uploadString(fileRef, append);
-        });
-      });
-    })
-    .catch(error => {
-      console.error('Get Download URL error: ', error);
-    });
+  const registerListRef = ref(database, 'registers');
+  const newRegisterRef = push(registerListRef);
+  const current = new Date();
+  return set(newRegisterRef, {
+    time: `${current.toLocaleTimeString()} - ${current.toLocaleDateString()}`,
+    name,
+    contact,
+  });
 };
 
 export default trigger;
